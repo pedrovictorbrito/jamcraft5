17ac137619623bfa3dcf034aa267b236f6f9b08f
defBig <pedrocga1@gmail.com>
2020-04-06 16:10:16 -0300

    Working prototype

--- src/main.c
+++ src/main.c
@@ -3,59 +3,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_OF_MOTHER_SPRITES 6
-
-struct GlobalState {
-    Uint32 ticksEllapsed;
-    const Uint32* keyboardState;
-    Uint32 lastArrowBtnPressed;
-};
-
-enum MotherSprites { FRONT1, FRONT2, BACK1, BACK2, LEFT, RIGHT };
-
-SDL_Texture* getMotherTexture(struct GlobalState globalState, SDL_Texture* textures[]) {
-    if (globalState.lastArrowBtnPressed == SDL_SCANCODE_DOWN) {
-        if ((globalState.ticksEllapsed / 1000) & 1 == 1) return textures[FRONT1];
-        else return textures[FRONT2];
-    }
-    if (globalState.lastArrowBtnPressed == SDL_SCANCODE_UP) {
-        if ((globalState.ticksEllapsed / 1000) & 1 == 1) return textures[BACK1];
-        else return textures[BACK2];
-    }
-    if (globalState.lastArrowBtnPressed == SDL_SCANCODE_LEFT) return textures[LEFT];
-    else return textures[RIGHT];
-}
-
 int main(int argc, char* argv[]) {
     SDL_Window* window;
     SDL_Renderer* renderer;
+    /*
     SDL_Surface* motherSurfaces[NUM_OF_MOTHER_SPRITES];
     SDL_Texture* motherTextures[NUM_OF_MOTHER_SPRITES];
-    SDL_Surface* spriteSheet, * frameCountSurface;
-    SDL_Rect motherSpriteDimensions = {
+    */
+    SDL_Texture* spriteSheet;
+    SDL_Surface* spriteSheetSurface, * frameCountSurface;
+    SDL_Rect spriteSelect = {
         .x = 0,
         .y = 0,
-        .w = 15,
-        .h = 14
+        .w = 20,
+        .h = 15
     }, mother = {
         .x = 0,
         .y = 0,
         .w = 1280 / 16,
         .h = 720 / 9
-    }, FPScounter = {
-        .x = 1280 / 16 * 15,
-        .y = 0,
-        .w = 1280 / 16,
-        .h = 720 / 9
     };
-    SDL_Texture* motherFrontTexture, *currentFrame;
     SDL_Event event;
-    register int frameCount = 0;
-    char* frameCountStr = (char*)malloc(3 * sizeof(char));
     const Uint8 *keyboardState;
     char* basePath;
     char* resPath ;
-    struct GlobalState globalState;
+    Uint32 ticksElapsed;
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
@@ -77,9 +49,9 @@ int main(int argc, char* argv[]) {
     resPath = (char*) malloc(SDL_utf8strlen(basePath) + 256 + 4);
     sprintf_s(resPath, SDL_utf8strlen(basePath) + SDL_strlen("res\\spriteSheet.bmp") + 1,"%sres\\spriteSheet.bmp", basePath);
     
-    spriteSheet = SDL_LoadBMP_RW(SDL_RWFromFile(resPath, "rb"), 1); 
+    spriteSheetSurface = SDL_LoadBMP_RW(SDL_RWFromFile(resPath, "rb"), 1); 
 
-    if (!spriteSheet) {
+    if (!spriteSheetSurface) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create sprite sheet surface from bitmap file: %s", SDL_GetError());
         SDL_DestroyWindow(window);
         SDL_DestroyRenderer(renderer);
@@ -87,85 +59,47 @@ int main(int argc, char* argv[]) {
         return 3;
     }
 
-    for (int sprite = FRONT1; sprite < NUM_OF_MOTHER_SPRITES; sprite++, motherSpriteDimensions.x += 15) {
-        motherSurfaces[sprite] = spriteSheet;
-        SDL_SetClipRect(motherSurfaces[sprite], &motherSpriteDimensions);
-    }
-    for (int sprite = FRONT1; sprite < NUM_OF_MOTHER_SPRITES; sprite++) {
-        motherTextures[sprite] = SDL_CreateTextureFromSurface(renderer, motherSurfaces[sprite]);
-    }
+    SDL_SetColorKey(spriteSheetSurface, SDL_TRUE, SDL_MapRGB(spriteSheetSurface->format, 255, 255, 255));
 
-    //mother_front = surface;
-    //SDL_BlitSurface(surface, &sprite, mother_front, NULL);
-    //TTF_Init();
-    // Font courtesy of Craftron Gaming
-    sprintf_s(resPath, SDL_strlen(basePath) + SDL_strlen("res\\Minecraft.ttf") + 1, "%sres\\Minecraft.ttf", basePath);
-    TTF_Font* font = TTF_OpenFont(resPath, 16);
-    SDL_Color white = { 255, 255, 255 };
-
-    //Mind you that (0,0) is on the top left of the window/screen, think a rect as the text's box, that way it would be very simple to understance
-
-    //Now since it's a texture, you have to put RenderCopy in your game loop area, the area where the whole code executes
-
-    for (int sprite = FRONT1; sprite < NUM_OF_MOTHER_SPRITES; sprite++) {
-        if (!motherTextures[sprite]) {
-            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
-            SDL_DestroyWindow(window);
-            SDL_DestroyRenderer(renderer);
-            for (int sprite = FRONT1; sprite < NUM_OF_MOTHER_SPRITES; sprite++) {
-                SDL_FreeSurface(motherSurfaces[sprite]);
-            }
-            SDL_Quit();
-            return 3;
-        }
-    }
-    for (int sprite = FRONT1; sprite < NUM_OF_MOTHER_SPRITES; sprite++) {
-        SDL_FreeSurface(motherSurfaces[sprite]);
-    }
+    spriteSheet = SDL_CreateTextureFromSurface(renderer, spriteSheetSurface);
 
     while (1) {
         SDL_PollEvent(&event);
         if (event.type == SDL_QUIT) {
             break;
         }
-        globalState.ticksEllapsed = SDL_GetTicks();
-        sprintf_s(frameCountStr, 3, "%d", globalState.ticksEllapsed / 1000);
-        frameCountSurface = TTF_RenderText_Solid(font, frameCountStr, white);
-        currentFrame = SDL_CreateTextureFromSurface(renderer, frameCountSurface);
-        globalState.keyboardState = SDL_GetKeyboardState(NULL);
-        if (globalState.keyboardState[SDL_SCANCODE_DOWN]) {
-            mother.y++;
-            globalState.lastArrowBtnPressed = SDL_SCANCODE_DOWN;
-        }
-        if (globalState.keyboardState[SDL_SCANCODE_UP]) {
-            mother.y--;
-            globalState.lastArrowBtnPressed = SDL_SCANCODE_UP;
-        }
-        if (globalState.keyboardState[SDL_SCANCODE_LEFT]) {
-            mother.x--;
-            globalState.lastArrowBtnPressed = SDL_SCANCODE_LEFT;
-        }
-        if (globalState.keyboardState[SDL_SCANCODE_RIGHT]) {
-            mother.x++;
-            globalState.lastArrowBtnPressed = SDL_SCANCODE_RIGHT;
-        }
-        if (globalState.keyboardState[SDL_SCANCODE_ESCAPE]) break;
+        ticksElapsed = SDL_GetTicks();
         SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer,
-            currentFrame,
-            NULL,
-            &FPScounter);
-        SDL_RenderCopy(renderer, getMotherTexture(globalState, motherTextures), NULL, &mother);
+        keyboardState = SDL_GetKeyboardState(NULL);
+        if (keyboardState[SDL_SCANCODE_DOWN]) {
+            mother.y++;
+            spriteSelect.x = 0;
+            SDL_RenderCopy(renderer, spriteSheet, &spriteSelect, &mother);
+        }
+        else if (keyboardState[SDL_SCANCODE_UP]) {
+            mother.y--;
+            spriteSelect.x = 40;
+            SDL_RenderCopy(renderer, spriteSheet, &spriteSelect, &mother);
+        }
+        else if (keyboardState[SDL_SCANCODE_RIGHT]) {
+            mother.x++;
+            spriteSelect.x = 80;
+            SDL_RenderCopy(renderer, spriteSheet, &spriteSelect, &mother);
+        }
+        else if (keyboardState[SDL_SCANCODE_LEFT]) {
+            mother.x--;
+            spriteSelect.x = 100;
+            SDL_RenderCopy(renderer, spriteSheet, &spriteSelect, &mother);
+        }
+        else if (keyboardState[SDL_SCANCODE_ESCAPE]) break;
+        else SDL_RenderCopy(renderer, spriteSheet, &spriteSelect, &mother);
         SDL_RenderPresent(renderer);
     }
-
-    for (int sprite = FRONT1; sprite < NUM_OF_MOTHER_SPRITES; sprite++)
-        SDL_DestroyTexture(motherTextures[sprite]);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-    //SDL_free(basePath);
-    //free(resPath);
+    SDL_free(basePath);
+    free(resPath);
     SDL_Quit();
 
     return 0;

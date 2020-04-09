c095e183b497509d7fc75faaf817263da32fae95
Pedro <pedrocga1@gmail.com>
2020-04-08 22:03:24 -0300

    Backup

--- src/main.c
+++ src/main.c
@@ -3,12 +3,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MOVE_DOWN(ENTITY, SPRITE_ONE, SPRITE_TWO) _move(&ticks_snapshot[0], &ENTITY, &ENTITY.y, 10, renderer, sprite_sheet, SPRITE_ONE, SPRITE_TWO)
+#define ANIMATION_TIME 400
+#define ANIMATION_KEY_FRAME_COUNT 4
+
+#define MOVE_DOWN(ENTITY_ADDRESS, SPRITE_ONE, SPRITE_TWO) _move(ticks_snapshot, ENTITY_ADDRESS, ENTITY_ADDRESS.y, 10, renderer, sprite_sheet, SPRITE_ONE, SPRITE_TWO)
+#define MOVE_UP(ENTITY_ADDRESS, SPRITE_ONE, SPRITE_TWO) _move(ticks_snapshot, ENTITY_ADDRESS, ENTITY_ADDRESS.y, -10, renderer, sprite_sheet, SPRITE_ONE, SPRITE_TWO)
+#define MOVE_RIGHT(ENTITY_ADDRESS, SPRITE_ONE, SPRITE_TWO) _move(ticks_snapshot, ENTITY_ADDRESS, ENTITY_ADDRESS.x, 10, renderer, sprite_sheet, SPRITE_ONE, SPRITE_TWO)
+#define MOVE_LEFT(ENTITY_ADDRESS, SPRITE_ONE, SPRITE_TWO) _move(ticks_snapshot, ENTITY_ADDRESS, ENTITY_ADDRESS.x, -10, renderer, sprite_sheet, SPRITE_ONE, SPRITE_TWO)
 
 typedef enum {
     MOTHER_FACING_DOWN_CRYING = 0,
-    MOTHER_FACING_DOWN_PLAIN = 20
+    MOTHER_FACING_DOWN_PLAIN = 20,
+    MOTHER_FACING_UP_ONE = 40,
+    MOTHER_FACING_UP_TWO = 60,
+    MOTHER_FACING_RIGHT_WALKING = 80,
+    MOTHER_FACING_RIGHT_STILL = 100,
+    MOTHER_FACING_LEFT_WALKING = 120,
+    MOTHER_FACING_LEFT_STILL = 140
 } Sprite;
+
+// Globals
+typedef struct G {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+} Global;
+
+Global global;
+
+void destroy_window_and_renderer() {
+    SDL_DestroyRenderer(global.renderer);
+    SDL_DestroyWindow(global.window);
+}
+
+inline void panic(char* msg) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, msg, SDL_GetError());
+    exit(EXIT_FAILURE);
+}
 
 SDL_Rect* get_sprite(Sprite sprite) {
     static SDL_Rect buffer_rect = {
@@ -21,20 +51,22 @@ SDL_Rect* get_sprite(Sprite sprite) {
     return &buffer_rect;
 }
 
-void _move(Uint32 *ticks_snapshot, SDL_Rect *entity, int *entity_property, int pixels_to_move, SDL_Renderer *renderer, SDL_Texture *sprite_sheet, Sprite first_sprite, Sprite second_sprite) {
-    while(SDL_GetTicks() - *(ticks_snapshot + 1) < 500) {
-        if ((SDL_GetTicks() - *ticks_snapshot - *(ticks_snapshot + 1)) >= 125) {
+void _move(Uint32 ticks_snapshot, SDL_Rect *entity, int *entity_property, int pixels_to_move, SDL_Renderer *renderer, SDL_Texture *sprite_sheet, Sprite first_sprite, Sprite second_sprite) {
+    int ms_elapsed = 0;
+    while(ms_elapsed < (ANIMATION_TIME / 2)) {
+        if ((SDL_GetTicks() - ticks_snapshot - ms_elapsed) >= ANIMATION_TIME / ANIMATION_KEY_FRAME_COUNT) {
             *entity_property += pixels_to_move;
-            ticks_snapshot[1] += 125;
+            ms_elapsed += ANIMATION_TIME / ANIMATION_KEY_FRAME_COUNT;
+            SDL_RenderClear(renderer);
             SDL_RenderCopy(renderer, sprite_sheet, get_sprite(first_sprite), entity);
             SDL_RenderPresent(renderer);
-            SDL_RenderClear(renderer);
         }
     }
-    while(SDL_GetTicks() - *ticks_snapshot < 100) {
-        if ((SDL_GetTicks() - *ticks_snapshot - *(ticks_snapshot + 1)) >= 125) {
+    while(ms_elapsed < ANIMATION_TIME) {
+        if ((SDL_GetTicks() - ticks_snapshot - ms_elapsed) >= ANIMATION_TIME / ANIMATION_KEY_FRAME_COUNT) {
             *entity_property += pixels_to_move;
-            ticks_snapshot[1] += 125;
+            ms_elapsed += ANIMATION_TIME / ANIMATION_KEY_FRAME_COUNT;
+            SDL_RenderClear(renderer);
             SDL_RenderCopy(renderer, sprite_sheet, get_sprite(second_sprite), entity);
             SDL_RenderPresent(renderer);
         }
@@ -42,37 +74,30 @@ void _move(Uint32 *ticks_snapshot, SDL_Rect *entity, int *entity_property, int p
 }
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
-        return 3;
-    }
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) panic("Couldn't initialize SDL: %s");
+    atexit(SDL_Quit);
 
-    if (TTF_Init() == -1) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize TTF: %s", TTF_GetError());
-        SDL_Quit();
-    }
+    if (TTF_Init() == -1) panic("Couldn't initialize TTF: %s");
+    atexit(TTF_Quit);
 
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-
+    SDL_Window window;
+    SDL_Renderer renderer;
     if (SDL_CreateWindowAndRenderer(1280, 720, SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED,
-        &window, &renderer) == -1) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
-        SDL_Quit();
-        return 3;
-    }
+        &window, &renderer) == -1) panic("Couldn't create window and renderer: %s");
+    atexit(destroy_window_and_renderer);
 
     char *base_path = SDL_GetBasePath();
     void *buffer = malloc(SDL_utf8strlen(base_path) + 4 + 256);
+
+    snprintf((char*)buffer, SDL_utf8strlen(base_path) + SDL_strlen("res?Minecraft.ttf") + 1, "%sres%cMinecraft.ttf", base_path, PATH_SEPARATOR);
+    TTF_Font *font = TTF_OpenFont(buffer, 16);
+
     snprintf((char*)buffer, SDL_utf8strlen(base_path) + SDL_strlen("res?sprite_sheet.bmp") + 1, "%sres%csprite_sheet.bmp", base_path, PATH_SEPARATOR);
 
     SDL_Surface* sprite_sheet_surface = SDL_LoadBMP_RW(SDL_RWFromFile(buffer, "rb"), 1);
 
     if (!sprite_sheet_surface) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create sprite sheet surface from bitmap file: %s", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_DestroyRenderer(renderer);
-        SDL_Quit();
         return 3;
     }
 
@@ -88,9 +113,10 @@ int main(int argc, char* argv[]) {
         .w = 1280 / 16,
         .h = 720 / 9
     };
+
     SDL_Event event;
     Uint8 *keyboard_state;
-    Uint32 ticks_snapshot[2];
+    Uint32 ticks_snapshot;
 
     while (SDL_TRUE) {
         SDL_PollEvent(&event);
@@ -100,28 +126,12 @@ int main(int argc, char* argv[]) {
 
         keyboard_state = SDL_GetKeyboardState(NULL);
 
-        ticks_snapshot[0] = SDL_GetTicks();
-        ticks_snapshot[1] = 0;
+        ticks_snapshot = SDL_GetTicks();
 
-        if (keyboard_state[SDL_SCANCODE_DOWN]) {
-            MOVE_DOWN(mother, MOTHER_FACING_DOWN_CRYING, MOTHER_FACING_DOWN_PLAIN);
-        }
-        /*
-        else if (keyboard_state[SDL_SCANCODE_UP]) {
-            mother.y -= 80;
-            spriteSelect.x = 40;
-            SDL_RenderCopy(renderer, sprite_sheet, &spriteSelec, &mother);
-        }
-        else if (keyboard_state[SDL_SCANCODE_RIGHT]) {
-            mother.x += 80;
-            spriteSelect.x = 80;
-            SDL_RenderCopy(renderer, sprite_sheet, &spriteSelect, &mother);
-        }
-        else if (keyboard_state[SDL_SCANCODE_LEFT]) {
-            mother.x -= 80;
-            spriteSelect.x = 100;
-            SDL_RenderCopy(renderer, sprite_sheet, &spriteSelect, &mother);
-        } */
+        if (keyboard_state[SDL_SCANCODE_DOWN]) MOVE_DOWN(&mother, MOTHER_FACING_DOWN_PLAIN, MOTHER_FACING_DOWN_CRYING);
+        else if (keyboard_state[SDL_SCANCODE_UP]) MOVE_UP(&mother, MOTHER_FACING_UP_ONE, MOTHER_FACING_UP_TWO);
+        else if (keyboard_state[SDL_SCANCODE_RIGHT]) MOVE_RIGHT(&mother, MOTHER_FACING_RIGHT_WALKING, MOTHER_FACING_RIGHT_STILL);
+        else if (keyboard_state[SDL_SCANCODE_LEFT]) MOVE_LEFT(&mother, MOTHER_FACING_LEFT_WALKING, MOTHER_FACING_LEFT_STILL);
         else if (keyboard_state[SDL_SCANCODE_ESCAPE]) break;
         else SDL_RenderCopy(renderer, sprite_sheet, get_sprite(MOTHER_FACING_DOWN_CRYING), &mother);
         SDL_RenderPresent(renderer);
